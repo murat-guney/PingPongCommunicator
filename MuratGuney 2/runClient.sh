fe77752322e6e4c622bd90186e26386788eafd95
murat-guney <141121665+murat-guney@users.noreply.github.com>
2024-05-03 21:01:05 +0300

    Delete MuratGuney 2 directory

--- MuratGuney 2/runClient.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Projeyi temizler ve derler
-mvn clean compile
-
-# PlayerClient sınıfını çalıştırır
-mvn exec:java -Dexec.mainClass=org.example.PlayerClient
--- MuratGuney 2/runLocal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mvn clean compile
-mvn exec:java -Dexec.mainClass=org.example.Main -Dexec.args="local"
